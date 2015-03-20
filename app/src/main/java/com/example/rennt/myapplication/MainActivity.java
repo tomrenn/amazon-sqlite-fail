3fa717207217a63a1b89ebe7992b29b4ab1eb973
Tom Renn <tom.renn@dowjones.com>
2015-03-20 11:42:28 -0400

    initial commit of amazon sqlite failure

---
+++ app/src/main/java/com/example/rennt/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.rennt.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.adobe.adms.measurement.ADMS_Measurement;
+
+import java.util.Locale;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Setting Locale breaks SQLite implementation
+        Locale.setDefault(Locale.KOREA);
+
+        // Tracking library uses SQLite, and crashes
+        ADMS_Measurement measurement = ADMS_Measurement.sharedInstance(this);
+
+
+        Log.i("MainActivity", "hello world!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
